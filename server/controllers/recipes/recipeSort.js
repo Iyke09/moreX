08744149a0e9dce474647a3f37640089d5477980
Emma Paris <iykay33@gmail.com>
2017-10-25 21:30:42 +0100

    add functionality to enable upvoting and downvoting

---
+++ server/controllers/recipes/recipeSort.js
@@ -0,0 +1,11 @@
+import recipes from '../../data/recipes'
+
+const sortRecipe = (req,res) => {
+  try{
+    res.send(recipes.sort(req.query.sort, req.query.order))
+  } catch (err){
+    res.send(err.message)
+  }
+}
+
+export default sortRecipe;
